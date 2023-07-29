30a2442cf98e7577aa95ffeb385e35eff5fd73eb
dilip-simbiotik <dilip.s@simbiotikgroup.com>
2023-07-29 14:57:34 +0530

    database configure

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const db = async () => {
   try {
     await mongoose
-      .connect("mongodb://127.0.0.1:27017/cloth-app");
+      .connect("mongodb+srv://social:5lFqEGVfkQVKFVql@cluster0.v3q7scl.mongodb.net/clothes_app");
     console.log("Connected to Database.");
   } catch (err) {
     console.log("Database not Connected.");
